0507926ae65e929cd824f8ffcba69879bc1bce74
kukumaj <zakumaj@gmail.com>
2022-08-13 13:42:55 +0200

    Done

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first;
+        int second;
+        int third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.print(first + "" + second + "" + third + "\n");
     }
 }
